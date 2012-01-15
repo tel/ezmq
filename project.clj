f1a6a7243516b76f4b70bcca42e7a6afabcd35d2
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-01-15 00:16:32 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ezmq "0.0.1"
+  :description "Yet another Clojure ZMQ wrapper for JZMQ."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [slingshot "0.10.1"]
+                 [cascalog/carbonite "1.0.5"]]
+  :dev-dependencies [[native-deps "1.0.5"]]
+  :native-dependencies [[org.clojars.starry/jzmq-native-deps "2.0.10"]]
+  :repl-options [:init nil :caught clj-stacktrace.repl/pst+])
---
+++ test/ezmq/test/core.clj
@@ -0,0 +1,6 @@
+(ns ezmq.test.core
+  (:use [ezmq.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
