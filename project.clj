816a4b4b372fd0c7ec5c7b475e43218281aea7f7
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-05-27 10:28:30 -0400

    * project.clj: upgrade to lein 2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ezmq "0.0.1"
   :description "Yet another Clojure ZMQ wrapper for JZMQ."
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [jspha/zmq-native "2.0.10.4"]
                  [slingshot "0.10.1"]
                  [cascalog/carbonite "1.0.5"]]
-  :dev-dependencies [[native-deps "1.0.5"]]
-  :native-dependencies [[org.clojars.starry/jzmq-native-deps "2.0.10"]]
-  :native-path "/usr/local/lib")
+  :native-path "native"
+  :resource-paths ["native"])
